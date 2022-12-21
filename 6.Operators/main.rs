e8c35db6ac73f361fac7d4a244452f93481d8987
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2022-12-21 21:55:03 +0530

    Operators Added

---
+++ 6.Operators/main.rs
@@ -0,0 +1,19 @@
+fn 
+main(){
+    let num1 = 5;
+    let num2 = 2;
+
+    println!("{}",num1+num2);
+    println!("{}",num1-num2);
+    println!("{}",num1*num2);
+    println!("{}",num1/num2);
+    println!("{}",num1%num2);
+
+
+    println!("{}",num1>num2);
+    println!("{}",num1>=num2);
+    println!("{}",num1<num2);
+    println!("{}",num1<=num2);
+    println!("{}",num1==num2);
+    println!("{}",num1!=num2);
+}
