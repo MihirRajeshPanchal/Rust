7723d7f435a96e804826355890a6790d49c099ce
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2022-12-21 21:20:46 +0530

    Variables Immutability added

---
+++ 3.Variables/main.rs
@@ -0,0 +1,16 @@
+fn
+main(){
+    // Immutable
+
+    let sal = 35000;
+    println!("My Salary is {}",sal);
+    // sal = 40000;
+    // println!("My Salary is {}",sal);
+   
+    //Mutable
+
+    let mut sal = 350000;
+    println!("My Salary is {}",sal);
+    sal = 400000;
+    println!("My Salary is {}",sal);
+}
