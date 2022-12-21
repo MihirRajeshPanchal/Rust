ae97c31b6ba0c82afe9e5af37017eb32c071fc12
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2022-12-21 21:17:29 +0530

    DataTypes added

---
+++ 2.DataTypes/main.rs
@@ -0,0 +1,12 @@
+fn
+main(){
+    let name = "Mihir";
+    let age = "18";
+    // let isMale = true; //no camel case in variables
+    let ismale = true;
+    let icon_char = '♥';
+    println!("My Name is : {}",name);
+    println!("My age is : {}",age);
+    println!("Is Male : {}",ismale);
+    println!("Heart : {}",icon_char);
+}
