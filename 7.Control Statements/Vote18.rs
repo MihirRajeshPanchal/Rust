c2eebcefe79d6613858b8d187a468f93979cee4e
MihirRajeshPanchal <mihirpanchal5400@gmail.com>
2022-12-22 18:10:01 +0530

    Control Statements added

---
+++ 7.Control Statements/Vote18.rs
@@ -0,0 +1,14 @@
+fn
+main(){
+    let age = 18;
+    if age > 18{
+        println!("Above 18");
+    }
+    else if age < 18{
+        println!("Below 18");
+    }
+    else{
+        println!("18");
+    }
+
+}
---
+++ 7.Control Statements/oddEven.rs
@@ -0,0 +1,10 @@
+fn
+main(){
+    let num = 13;
+    if num%2==0{
+        println!("Number is Even");
+    }
+    else{
+        println!("Number is Odd");
+    }
+}
